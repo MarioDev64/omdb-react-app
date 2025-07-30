49b1177733ac09543084663cafc5ec08ae44739f
Mario Torres <marioytorres@hotmail.com>
2025-07-29 20:49:18 -0400

    feat: configure SSR for dynamic routes and add 404 page

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 export default [
   index('routes/home.tsx'),
   route('movie/:id', 'routes/movie.tsx'),
+  route('404', 'routes/404.tsx'),
 ] satisfies RouteConfig;
